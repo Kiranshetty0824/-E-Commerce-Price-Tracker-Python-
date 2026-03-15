814069ecb90ed4805ef19305d9480886e5ec850b
kiran shetty <kirushetty194@gmail.com>
2026-03-15 21:38:33 +0530

    Update E-Commerce Price Tracker (Python).py

--- E-Commerce Price Tracker (Python).py
+++ E-Commerce Price Tracker (Python).py
@@ -6,7 +6,7 @@ from datetime import datetime
 import os
 
 
-
+ 
 # -------- SETTINGS --------
 URL = "https://webscraper.io/test-sites/e-commerce/static/product/569"  # Demo product URL
 HEADERS = {"User-Agent": "Mozilla/5.0"}
